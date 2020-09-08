fafcefb1094dfc6c02aa3b718eabbbbc32e0fb43
Andreas Ravn <56455491+Andreas-Ravn@users.noreply.github.com>
2020-09-08 10:59:48 +0200

    Add files via upload

---
+++ opgave1.java
@@ -0,0 +1,7 @@
+public class opgave1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning java now");
+        System.out.println("Programing is fun");
+    }
+}
